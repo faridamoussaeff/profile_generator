1f7a23e79dce7fde1322accadb651ff3d43dd2e4
Farida <faridamus@protonmail.com>
2021-11-03 02:28:01 +0000

    Revise and correct survey after eslint

--- survey.js
+++ survey.js
@@ -5,33 +5,22 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-  rl.question('Which sport is your absolute favourite?', (answer) => {
-    console.log(`My favourite sport: ${answer}`);
-    rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
-      console.log(`I have many skills, but my superpower is: ${answer}`);
-      rl.question('What do you listen to while doing that?', (answer) => {
-        console.log(`I enjoy listening music while coding, in particular: ${answer}`);
-        rl.question('Which meal is your favourite (eg: dinner, brunch, etc.)?', (answer) => {
-          console.log(`I am a foodie, but I love the most: ${answer}`);
-
+rl.question('Where do you like to travel for your vacation?', (answer1) => {
+  console.log(`I love to travell, but my favourite destination is: ${answer1}`);
+  rl.question('How many languages do you speak?', (answer2) => {
+    console.log(`I can speak: ${answer2}`);
+    rl.question('Which meal is your favourite (eg: dinner, brunch, etc)?', (answer3) => {
+      console.log(`I think, my favourite meal is: ${answer3}`);
+      rl.question('Do you like spending your vacations at home or in a foreign country?', (answer4) => {
+        console.log(`I prefer to spend my vacation in: ${answer4}`);
+        rl.question('Which sport is your absolute favourite?', (answer5) => {
+          console.log(`Oh, my absolute favourite is probably: ${answer5}`);
+          rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer6) => {
+            console.log(`I have many skills, but my superpower is: ${answer6}`);
+            rl.close();
+          });
+        });
+      });
+    });
   });
-
-  rl.close();
 });
-}
-
-// The rl.question() method displays the query by writing it to the output, 
-// waits for user input to be provided on input, then invokes the callback function passing the provided input as the first argument.
-// When called, rl.question() will resume the input stream if it has been paused.
-
-
-// Instead of asking the user about what they think of Node.js, we can ask them the following questions, in order:
-// What's your name? Nicknames are also acceptable :)
-// What's an activity you like doing?
-// What do you listen to while doing that?
-// Which meal is your favourite (eg: dinner, brunch, etc.)
-// What's your favourite thing to eat for that meal?
-// Which sport is your absolute favourite?
-// What is your superpower? In a few words, tell us what you are amazing at
